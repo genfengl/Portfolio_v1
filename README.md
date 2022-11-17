79882a4793abfb9e5c6341a287d4a7650f6c52cf
genfengl <genfeng.liu@gmail.com>
2022-11-17 22:40:10 +0800

    adding things to navbar

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Portfolio_v1
 
+## Link to the porfolio: [Porfolio] (https://geraldliu-dev.netlify.app/ "The Portfolio")
+
 ### Creating a porfolio website using vite.js and react.js
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,23 @@ const Navbar = () => {
         // Navbar
         <nav className="py-6 px-6 mt-6 sticky top-0 sm:mx-6">
             {/* Flex container */}
-            <div className="flex align-middle justify-between">
+            <div className="flex justify-between">
                 {/* Brand */}
                 <div className="font-bold text-2xl flex gap-2 items-center">
                     <img src="/logo.gif" className="h-6 w-6" />  GeraldLiu
+                </div>
+                {/* Navlinks */}
+                <div className="flex justify-between gap-4 items-center">
+
+                    <div className="col-start-7 col-span-1 text-l">
+                        Skills
+                    </div>
+                    <div className="col-start-8 col-span-1 ">
+                        Projects
+                    </div>
+                    <div className="col-start-9 col-span-1">
+                        Contact
+                    </div>
                 </div>
             </div>
         </nav>
