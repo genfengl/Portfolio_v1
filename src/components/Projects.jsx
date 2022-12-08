d046043201f4f6544684c4de1bf2a554ca8a7982
genfengl <genfeng.liu@gmail.com>
2022-12-08 15:44:02 +0800

    added links and adjusted responsive designs of projects components

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,54 +10,66 @@ const Projects = () => {
                 </div>
                 {/* Projects container */}
                 <div className="grid grid-cols-[345px] justify-center items-center gap-2 place-content-center place-items-center 
-                xs:grid-cols-[360px] md:grid-cols-[360px_360px] lg:grid-cols-[414px_414px] xl:grid-cols-3">
+                xs:grid-cols-[360px] md:grid-cols-[360px_360px] lg:grid-cols-[414px_414px] xl:grid-cols-[414px_414px_414px]
+                2xl:grid-cols-[498px_498px_498px]">
                     {/* ChatUp! */}
+
                     <div className="w-full aspect-square relative group">
-                        <div className="bg-[#7289da] w-full aspect-square z-10 p-2 rounded-xl text-word 
+                        <a href="https://chatup-56.netlify.app/">
+                            <div className="bg-[#7289da] w-full aspect-square z-10 p-2 rounded-xl text-word 
                         flex items-center flex-col justify-center gap-1 absolute ease-in-out duration-300
                         group-hover:opacity-0">
-                            <div className="text-3xl font-bold">ChatUp!</div>
-                            <div className="text-xl">Instant messenger app</div>
-                        </div>
-                        <img src="/ChatUp_Screenshot.png" 
-                        className="absolute w-[310px] top-[1px] left-[1px] aspect-square rounded-xl
-                        xs:w-[358px]"></img>
+                                <div className="text-3xl font-bold">ChatUp!</div>
+                                <div className="text-xl">Instant messenger app</div>
+                            </div>
+
+                            <img src="/ChatUp_Screenshot.png"
+                                className="absolute w-[310px] top-[1px] left-[1px] aspect-square rounded-xl
+                        xs:w-[358px] lg:w-[412px] 2xl:w-[496px]"></img>
+                        </a>
                     </div>
+
                     {/* NTMY */}
                     <div className="w-full aspect-square relative group">
-                        <div className="bg-[#C02433] w-full aspect-square z-10 p-2 rounded-xl text-word 
+                        <a href="https://butcherstorereactapp.fly.dev/">
+                            <div className="bg-[#C02433] w-full aspect-square z-10 p-2 rounded-xl text-word 
                         flex items-center flex-col justify-center gap-1 absolute ease-in-out duration-300
                         group-hover:opacity-0">
-                            <div className="text-3xl font-bold">NTMY</div>
-                            <div className="text-xl">E-commerce Store</div>
-                        </div>
-                        <img src="/NTMY_Screenshot.png" 
-                        className="absolute w-[310px] top-[1px] left-[1px] aspect-square rounded-xl
-                        xs:w-[358px]"></img>
+                                <div className="text-3xl font-bold">NTMY</div>
+                                <div className="text-xl">E-commerce Store</div>
+                            </div>
+                            <img src="/NTMY_Screenshot.png"
+                                className="absolute w-[310px] top-[1px] left-[1px] aspect-square rounded-xl
+                        xs:w-[358px] lg:w-[412px] 2xl:w-[496px]"></img>
+                        </a>
                     </div>
                     {/* Tic-Tac-Toe */}
                     <div className="w-full aspect-square relative group">
-                        <div className="bg-[#E83B2B] w-full aspect-square z-10 p-2 rounded-xl text-word 
+                        <a href="https://genfengl.github.io/TicTacToe/">
+                            <div className="bg-[#E83B2B] w-full aspect-square z-10 p-2 rounded-xl text-word 
                         flex items-center flex-col justify-center gap-1 absolute ease-in-out duration-300
                         group-hover:opacity-0">
-                            <div className="text-3xl font-bold">Tic-Tac-Toe</div>
-                            <div className="text-xl">Webpage Game</div>
-                        </div>
-                        <img src="/TicTacToe_Screenshot.png" 
-                        className="absolute w-[310px] top-[1px] left-[1px] aspect-square rounded-xl
-                        xs:w-[358px]"></img>
+                                <div className="text-3xl font-bold">Tic-Tac-Toe</div>
+                                <div className="text-xl">Webpage Game</div>
+                            </div>
+                            <img src="/TicTacToe_Screenshot.png"
+                                className="absolute w-[310px] top-[1px] left-[1px] aspect-square rounded-xl
+                        xs:w-[358px] lg:w-[412px] 2xl:w-[496px]"></img>
+                        </a>
                     </div>
                     {/* Tangerine's Cat Food Catalogue */}
                     <div className="w-full aspect-square relative group">
-                        <div className="bg-[#0D6EFD] w-full aspect-square z-10 p-2 rounded-xl text-word 
+                        <a href="https://tangerinecatfoodcatalogue.onrender.com/catfoods">
+                            <div className="bg-[#0D6EFD] w-full aspect-square z-10 p-2 rounded-xl text-word 
                         flex items-center flex-col justify-center gap-1 absolute ease-in-out duration-300
                         group-hover:opacity-0">
-                            <div className="text-3xl font-bold text-center">Tangerine's Cat Food Catalogue</div>
-                            <div className="text-xl">CRUD App</div>
-                        </div>
-                        <img src="/TangerinesCatFoodCatalogue_Screenshot.png" 
-                        className="absolute w-[310px] top-[1px] left-[1px] aspect-square rounded-xl
-                        xs:w-[358px]"></img>
+                                <div className="text-3xl font-bold text-center">Tangerine's Cat Food Catalogue</div>
+                                <div className="text-xl">CRUD App</div>
+                            </div>
+                            <img src="/TangerinesCatFoodCatalogue_Screenshot.png"
+                                className="absolute w-[310px] top-[1px] left-[1px] aspect-square rounded-xl
+                        xs:w-[358px] lg:w-[412px] 2xl:w-[496px]"></img>
+                        </a>
                     </div>
                 </div>
             </div>
